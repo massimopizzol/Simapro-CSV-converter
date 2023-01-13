a2972c5cd27ee2f8a5ad58a00c50741d35b8b487
massimopizzol <massimo@plan.aau.dk>
2023-01-13 22:06:37 +0100

    updates spcsv_comm_line.py to improve command call

--- spcsv_comm_line.py
+++ spcsv_comm_line.py
@@ -1,15 +1,19 @@
-
 """
-Created on Fri Jan 13 21:57:00 2023
+Created on Fri Jan 13 22:00:00 2023
 
 @author: massimo
 """
 
 from spcsv import *
+from sys import argv
 import pandas as pd
 
-for sheet in pd.ExcelFile('LCI_Example.xlsx').sheet_names:
+script, input_name = argv
+"""Specify the name of the file to be converted in .csv"""
+"""This script allows converting multiple sheets of an Excel file"""
 
-    spcsv_ready = pd.read_excel('LCI_Example.xlsx', sheet, index_col = False, header = None)
+for sheet in pd.ExcelFile(input_name).sheet_names:
+
+    spcsv_ready = pd.read_excel(input_name, sheet, index_col = False, header = None)
 
     to_spcsv(spcsv_ready, sheet +'.csv')
